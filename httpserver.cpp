9d9fa839a532f4be0894a3b2cb0758e01fad2645
Schroederdan <Schroeda@mail.gvsu.edu>
2018-11-28 22:45:36 -0500

    501 and 404 works

--- httpserver.cpp
+++ httpserver.cpp
@@ -20,10 +20,23 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <time.h>
+#include <ctime>
 #include <cstring>
 
+struct info{
+	int clientsocket;
+	char *dir;
+	bool usefile;
+	char *wfile;
+};
+
 void* handleclient(void* arg) {
-	int clientsocket = *(int*)arg;
+	struct info arginfo = *(struct info*)arg;
+	int clientsocket = arginfo.clientsocket;
+	char *dir = arginfo.dir;
+	bool usefile = arginfo.usefile;
+	char *wfile = arginfo.wfile;
+	
 	char line[5000];
 	
 	int rsize;
@@ -31,16 +44,30 @@ void* handleclient(void* arg) {
 	const char *head;
     const char *tail;
 
+	std::string ext;
+
+	bool e501;
+
 	std::string parse;
 	std::string type;
 	std::string file;
 	std::string connection;
+	std::string version;
+
+
+	std::string sendb;
+	char message[18];
+
+	time_t rawtime;
+	struct tm * timeinfo;
+  	char date[80];
+	
+	struct stat result;
 
 	while(1){
+		e501 = false;
 		rsize = recv(clientsocket, line, 5000, 0);
 		if(rsize == 0){
-			EVP_cleanup();
-            ERR_free_strings();
             pthread_exit(0);
 			return 0;
 		}
@@ -50,14 +77,16 @@ void* handleclient(void* arg) {
 		
 		counter = 0;
 		while(*tail != '\0'){
-			while (*tail != '\0' && *tail != ' '){
+			while (*tail != '\0' && *tail != ' ' && *tail != '\n' ){
 				parse = std::string(head, tail+1);
 				++tail;
 			}
+
 			if(counter==0){
 				type = parse;
 				if(strcmp(type.c_str(),"GET")!=0){
 					std::cout << "ERROR 501" << std::endl;
+					e501 = true;
 				}
 				counter++;	
 				std::cout<<"TYPE: " << type <<std::endl;
@@ -65,20 +94,68 @@ void* handleclient(void* arg) {
 				file = parse;
 				counter++;		
 				std::cout<<"FILE: " << file <<std::endl;	
-			}else if(counter==15){
-				connection = parse;
+			}else if(counter==2){
+				version = parse;
+				counter++;		
+				std::cout<<"VERSION: " << version <<std::endl;
+			}else if(counter==24){ 
+				connection = parse; // 15 in data comms on 11/27
 				counter++;
 				std::cout<<"CONNECTION: " << connection <<std::endl;
 			}else{
 				counter++;
 			}
-			
+
 			//std::cout<<parse<<std::endl;
 			tail++;		
 			head=tail;
 		}
 		
+		ext = file.substr(file.find("."),file.size());
+		std::cout<<"FILE EXT: " << ext <<std::endl;
+		if(ext != ".html" && ext != ".txt" && ext != ".jpg" && ext != ".pdf"){
+			std::cout << "ERROR 501" << std::endl;
+			e501 = true;
+		}
 
+		time (&rawtime);
+  		timeinfo = localtime(&rawtime);
+		strftime(date,sizeof(date),"%d-%m-%Y %H:%M:%S",timeinfo);
+		std::cout<<"DATE: " << date <<std::endl;
+
+		if(e501){
+			sendb = "";
+			strcpy(message,"<h1>501 Error</h1>");
+			
+			sendb = version + " 501 Not Implemented\nDate: " + date + "\nContent-Type: text/html; charset=utf-8\nContent-Length: 18\n\n" + message;  
+			send(clientsocket,sendb.c_str(), sendb.length(), 0);
+			pthread_exit(0);
+		}
+
+		char fname[50];
+		strcpy(fname,file.c_str());
+		std::FILE* myfile; 
+		if((myfile = std::fopen(strcat(dir,fname),"r")) == NULL){
+			sendb = "";
+			strcpy(message,"<h1>404 Error</h1>");
+			
+			sendb = version + " 404 Not Found\nDate: " + date + "\nContent-Type: text/html; charset=utf-8\nContent-Length: 18\n\n" + message;  
+			send(clientsocket,sendb.c_str(), sendb.length(), 0);
+			pthread_exit(0);
+		}
+		
+		time_t mod_time;
+		if(stat(dir, &result)==0){
+			mod_time = result.st_mtime;
+		}
+
+		char* lmodtime = std::ctime(&mod_time);
+		std::cout<<"MODTIME: " << lmodtime << std::endl;
+	
+		// SEND CODE 200 With FIle Contents
+		// WRITE TO TXT FILE / STDOUT
+		// 304 ERROR CODE
+		
 	}
 	return 0;
 }
@@ -154,17 +231,23 @@ int main(int arc, char** argv) {
 
 	listen(sockfd,10);
 
+	struct info clientinfo;
+	clientinfo.dir = dir;
+	clientinfo.usefile = usefile;
+	clientinfo.wfile = file;  
+
 	while(1){
+		
         int len = sizeof(clientaddr);
         int clientsocket = accept(sockfd, (struct sockaddr*)&clientaddr, (socklen_t*)&len);
         
+		clientinfo.clientsocket = clientsocket; 
+
         pthread_t child;
-        pthread_create(&child,NULL,handleclient,&clientsocket);
+        pthread_create(&child,NULL,handleclient,&clientinfo);
         pthread_detach(child);
 	
     }
-	EVP_cleanup();
-  	ERR_free_strings();
 	
 	std::cout << "End" << std::endl ;
 	
